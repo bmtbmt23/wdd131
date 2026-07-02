0ad8bb85c2e5faec4276ad5dcce74bbbfe9e3961
bmtbmt23 <bbeck1@byupathway.edu>
2026-07-01 21:26:19 -0600

    week1

--- Week1 copy/scripts/getdates.js
+++
@@ -1,3 +0,0 @@
-const year = new Date().getFullYear();
-document.getElementById("currentyear").textContent = year;
-document.getElementById("lastmodified").textContent ="LAst Modified: " + document.lastModified;

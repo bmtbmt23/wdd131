667e97313c1e22c48dadfb2495ffb6b40c579adc
bmtbmt23 <bbeck1@byupathway.edu>
2026-07-15 14:14:16 -0600

    week3

---
+++ scripts/place.js
@@ -0,0 +1,24 @@
+let today = new Date();
+let year = today.getFullYear();
+document.getElementById("currenteyear").innerHTML = year;
+document.getElementById("lastModified").innerHTML =
+"Last Modified: " + document.lastModified;
+
+const temperature = 9;
+const windSpeed = 7;
+
+function calculateWindChill(temp, speed){
+  return(
+    13.12 +
+    0.6215 * temp -
+    11.37 * Math.pow(speed,0.16) +
+    0.3965 * temp * Math.pow(speed,0.16)
+    ).toFixed(1);
+
+}
+
+if (temperature <= 10 && windSpeed > 4.48){
+  document.getElementById("windChill").textContent = `${calculateWindChill(temperature, windSpeed)} °C`;
+}else{
+  document.getElementById("windChill").textContet = "N/A";
+}
--- scripts/temples.js
+++ scripts/temples.js
@@ -1,6 +1,6 @@
 let today = new Date();
 let year = today.getFullYear();
-document.getElementById("currentyear").innerHTML = year;
+document.getElementById("currentYear").innerHTML = year;
 document.getElementById("lastModified").innerHTML =
 "Last Modified: " + document.lastModified;
 

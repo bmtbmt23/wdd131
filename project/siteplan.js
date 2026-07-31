80eca9136d8567d568940c1e78073eb780a90533
bmtbmt23 <bbeck1@byupathway.edu>
2026-07-30 20:01:51 -0600

    week05

---
+++ project/siteplan.js
@@ -0,0 +1,4 @@
+let d = new Date();
+
+document.getElementById("currentYear").innerHTML =`&copy;${d.getFullYear()} Bruna Beck`;
+document.getElementById("lastModified").textContent=`lastModified:${document.lastModified}`;

f038f133184007743e611434dc9d6f0fb7d19da7
bmtbmt23 <bbeck1@byupathway.edu>
2026-07-31 17:39:44 -0600

    week05

--- js/form.js
+++ js/form.js
@@ -3,11 +3,11 @@ let d = new Date();
 document.getElementById("currentYear").innerHTML = `&copy;${d.getFullYear()} Bruna Beck`;
 document.querySelector(`#lastModified`).textContent = `lastModified:${document.lastModified}`;
 
-const select = documet.querySelector("#product");
+const select = document.querySelector("#product");
 products.forEach(product =>{
   const option = document.createElement("option");
  
-  option.value = producti.id;
+  option.value = product.id;
   option.textContent = product.name;
   select.appendChild(option);
 });
